c41f5701a95cf75319d65cab058e8d9bb46997ee
Cydnee Owens <cks410@yahoo.com>
2020-09-10 09:34:31 -0600

    Add add_exhibits tests

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -15,4 +15,21 @@ class MuseumTest < Minitest::Test
     assert_instance_of Museum, dmns
     assert_equal "Denver Museum of Nature and Science", dmns.name
   end
+
+  def test_is_can_add_exhibits
+    dmns = Museum.new("Denver Museum of Nature and Science")
+    gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+    imax = Exhibit.new({name: "IMAX",cost: 15})
+
+    assert_equal [], dmns.exhibits
+
+    dmns.add_exhibit(gems_and_minerals)
+
+    dmns.add_exhibit(dead_sea_scrolls)
+
+    dmns.add_exhibit(imax)
+
+    assert_equal [gems_and_minerals, dead_sea_scrolls, imax], dmns.exhibits
+  end
 end
